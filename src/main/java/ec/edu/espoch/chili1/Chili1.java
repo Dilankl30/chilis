2a3f0a196ec435f5248c2ce62f6c1f8bdfa8c943
Dilankl30 <lucerodk7@gmail.com>
2024-11-05 09:57:02 -0500

    prueba

---
+++ src/main/java/ec/edu/espoch/chili1/Chili1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.chili1;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Chili1 {
+
+    public static void main(String[] args) {
+        System.out.println("chilisssss");
+    }
+}
